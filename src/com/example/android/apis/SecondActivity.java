3efc42e75057e1af83f5d3fcb64e5e06dd087ebe
kommineni <veeru.kommineni@gmail.com>
2015-03-02 16:31:03 +0530

    Add coding comments

--- src/com/example/android/apis/SecondActivity.java
+++ src/com/example/android/apis/SecondActivity.java
@@ -1,7 +1,16 @@
 package com.example.android.apis;
 
+import com.example.android.google.apis.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
-public class SecondActivity extends Activity{
-
+public class SecondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		System.out.println("Hai ");
+		System.out.println("Hello ");
+	}
 }
