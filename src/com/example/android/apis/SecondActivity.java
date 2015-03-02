2cff1f741342aab187ce91576cf55344c0d6a5a9
kommineni <veeru.kommineni@gmail.com>
2015-03-02 16:14:18 +0530

    Add onCreate()

--- src/com/example/android/apis/SecondActivity.java
+++ src/com/example/android/apis/SecondActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.apis;
 
+import com.example.android.google.apis.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
-public class SecondActivity extends Activity{
-
+public class SecondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
