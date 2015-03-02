68fef3b00624e3ae8c67837855a8ab7536784309
kommineni <veeru.kommineni@gmail.com>
2015-03-02 15:50:43 +0530

    Moving Latest code into staging

--- src/com/example/android/apis/SecondActivity.java
+++ src/com/example/android/apis/SecondActivity.java
@@ -1,17 +1,7 @@
 package com.example.android.apis;
 
-/**
- * Import statements
- */
-import com.example.android.google.apis.R;
-
 import android.app.Activity;
-import android.os.Bundle;
 
-public class SecondActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.second);
-	}
+public class SecondActivity extends Activity{
+
 }
